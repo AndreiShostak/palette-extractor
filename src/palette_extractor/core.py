0f9fe438c7df92007bfd8090f6e1cd72b7f5096f
Andrei Shostak <andrei.shostak@icloud.com>
2026-07-23 18:49:26 +0300

    feat(core): add extract method and random_state parameter

--- src/palette_extractor/core.py
+++ src/palette_extractor/core.py
@@ -1,11 +1,15 @@
 import numpy as np
 from skimage import io, transform
+from sklearn.cluster import KMeans
 
 
 class PaletteExtractor:
-    def __init__(self, n_colors: int = 5, target_size: int = 300) -> None:
+    def __init__(
+        self, n_colors: int = 5, target_size: int = 300, random_state: int = 8
+    ) -> None:
         self.n_colors = n_colors
         self.target_size = target_size
+        self.random_state = random_state
 
     def _load_image(self, image_path: str) -> np.ndarray:
         img = io.imread(image_path)
@@ -20,3 +24,21 @@ class PaletteExtractor:
         resized = transform.resize(img, (new_H, new_W), order=0)
 
         return resized
+
+    def extract(self, image_path: str):
+        img = self._load_image(image_path)
+
+        X = img.reshape((-1, 3))
+
+        kmeans = KMeans(self.n_colors, random_state=self.random_state)
+
+        kmeans.fit(X)
+
+        centers = kmeans.cluster_centers_
+
+        colors = centers * 255
+
+        labels = kmeans.labels_
+        counts = np.bincount(labels)
+
+        return {"colors": colors, "counts": counts}
